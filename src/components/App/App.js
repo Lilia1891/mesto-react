ef3aefa6f64a656d1a2ad02cdf2039ca41179fb0
Lilia Sviridova <svirivoda@gmail.com>
2022-08-06 16:07:50 +0300

    Create PopupWithForm component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,7 +10,68 @@ function App() {
       <Header />
       <Main />
       <Footer />
-      <PopupWithForm />
+      <PopupWithForm name="avatar" title="Обновить аватар">
+        <input
+          type="url"
+          name="avatar"
+          id="avatar"
+          placeholder="Ссылка на картинку"
+          className="popup__input"
+          required
+        />
+        <span className="popup__input-error popup__input-error_avatar"></span>
+      </PopupWithForm>
+      <PopupWithForm name="profile" title="Редактировать профиль">
+        <input
+          className="popup__input popup__input_type_name"
+          type="text"
+          name="title"
+          id="input-name"
+          placeholder="Имя"
+          minLength="2"
+          maxLength="40"
+          required
+        />
+        <span className="popup__input-error popup__input-error_title"></span>
+
+        <input
+          className="popup__input popup__input_type_job"
+          type="text"
+          name="job"
+          id="input-job"
+          placeholder="Вид деятельности"
+          minLength="2"
+          maxLength="200"
+          required
+        />
+        <span className="popup__input-error popup__input-error_job"></span>
+      </PopupWithForm>
+      <PopupWithForm name="add-card" title="Новое место">
+        <input
+          className="popup__input popup__input_type_place"
+          type="text"
+          placeholder="Название"
+          name="place"
+          id="input-place"
+          minLength="2"
+          maxLength="30"
+          required
+        />
+        <span className="popup__input-error popup__input-error_place"></span>
+
+        <input
+          className="popup__input popup__input_type_link"
+          type="url"
+          placeholder="Ссылка на картинку"
+          name="link"
+          id="input-link"
+          required
+        />
+        <span className="popup__input-error popup__input-error_link"></span>
+      </PopupWithForm>
+      <PopupWithForm name="confirm" title="Вы уверены?">
+        {" "}
+      </PopupWithForm>
 
       <div className="popup popup_view-image">
         <figure className="popup__figure">
